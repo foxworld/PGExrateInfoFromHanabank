921bd48f4ebe893a21b4f93de83fc760db95311e
이훈구 <foxworld@gmail.com>
2025-07-08 18:10:51 +0900

    달력테이블생성

--- docs/schema.sql
+++ docs/schema.sql
@@ -9,7 +9,15 @@ create table pgexrate (
 create unique index pgexrate_pk on pgexrate(tr_date, money_code, ex_sele);
 
 create table pgusd01 (
-tr_date char(8) not null,
-usd_rate decimal(6,2) not null
+    tr_date char(8) not null,
+    usd_rate decimal(6,2) not null
 );
 create unique index pgusd01_pk on pgusd01(tr_date);
+
+create table pgcal02 (
+    country_code char(3) not null,
+    trd_date  char(8) not null,
+    day_of_week char(1) not null,
+    legal_holiday char(8) default 'N' not null
+);
+create unique index pgcal02_pk on pgcal02(country_code,trd_date);

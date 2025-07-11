b9bcc8a0673bbaa40c8c2f2b470cb92119b848c0
이훈구 <foxworld@gmail.com>
2025-07-11 10:25:53 +0900

    달력로직제거

--- src/main/java/ksnet/pginfo/pgexrate/repository/JpaPgCal02Repository.java
+++
@@ -1,6 +0,0 @@
-package ksnet.pginfo.pgexrate.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaPgCal02Repository extends JpaRepository<PgCal02, PgCal02Key> {
-}

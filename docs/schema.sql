c11f6dda4475ad5cbfedccf449739db209a45ba3
이훈구 <foxworld@gmail.com>
2025-07-11 10:40:30 +0900

    달력로직제거

--- docs/schema.sql
+++ docs/schema.sql
@@ -13,11 +13,3 @@ create table pgusd01 (
     usd_rate decimal(6,2) not null
 );
 create unique index pgusd01_pk on pgusd01(tr_date);
-
-create table pgcal02 (
-    country_code char(3) not null,
-    trd_date  char(8) not null,
-    day_of_week char(1) not null,
-    legal_holiday char(8) default 'N' not null
-);
-create unique index pgcal02_pk on pgcal02(country_code,trd_date);
